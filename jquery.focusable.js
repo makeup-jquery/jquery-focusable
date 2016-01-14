850995bc588144b6d9cbb786523549e6f9c85f22
Ian McBurnie <imcburnie@ebay.com>
2016-01-14 15:41:52 -0800

    Bumped js version number

--- jquery.focusable.js
+++ jquery.focusable.js
@@ -1,7 +1,7 @@
 /**
 * @function $.fn.focusable
 * @name jquery-focusable
-* @version 0.0.4
+* @version 0.0.5
 * @author Ian McBurnie <ianmcburnie@hotmail.com>
 * @desc Finds all focusable descendant elements of the given selector and returns
 * them as a new jQuery object
